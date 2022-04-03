c770ae23e45ad0e1205128ad27997d82c69546dd
Jay Miller <kjaymiller@gmail.com>
2022-04-02 20:57:12 -0700

    remove unecessary files for db mgmt

--- download.py
+++
@@ -1,9 +0,0 @@
-from connection import es_client
-from pathlib import Path
-import json
-import typer
-
-match = {"match_all": {}}
-result = es_client.search(index="diversity-orgs", query=match, size=2000)
-
-Path('diversity-orgs.json').write_text(json.dumps([x['_source'] for x in result['hits']['hits']], indent=2))
